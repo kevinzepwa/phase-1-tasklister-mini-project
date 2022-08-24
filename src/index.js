ad7f4840f820d288b76fe8d183aab77c91cf97cc
kevinzepwa <zepwakevin8@gmail.com>
2022-08-24 20:15:30 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+    document.querySelector("#create-task-form").addEventListener("submit", (event) => {
+      event.preventDefault();
+  });
 });
+
